55d863c7b21140c69945d3bd9fc402377f768bdb
Bohan Zhang <b327zhan@uwaterloo.ca>
2024-05-18 14:57:28 -0400

    clean

--- src/simulation/Condition.py
+++ src/simulation/Condition.py
@@ -13,32 +13,6 @@ class Sat_condition:
             "satid": self.satid
         }
 
-
-# This is a collection of Sat_condition
-class UE_condition2:
-    def __init__(self, list_Sat_condition_json, ueid, creation_time):
-        self.ueid = ueid
-        self.conditions = {}
-        for condition in list_Sat_condition_json:
-            assert (self.ueid == condition["ueid"])
-            self.conditions[condition['satid']] = (Sat_condition(access_delay=condition['access_delay'],
-                                                 ueid=condition['ueid'],
-                                                 satid=condition['satid'],
-                                                 sourceid=condition['source']))
-        self.creation_time = creation_time
-
-    def available_access_conditions(self, time):
-        available_conditons = []
-        left_conditons = []
-        for sat_id in self.conditions:
-            condition = self.conditions[sat_id]
-            if self.creation_time + condition.access_delay == time:
-                available_conditons.append(condition)
-            elif self.creation_time + condition.access_delay > time:
-                left_conditons.append(condition)
-        return available_conditons, left_conditons
-
-
 class UE_condition:
     def __init__(self, targetid, delay, now):
         self.targetid = targetid
