a1a88086891d9a22518c41e00e4be68904681ca2
Bohan Zhang <b327zhan@uwaterloo.ca>
2024-05-27 16:16:55 -0400

    aggregate

--- src/simulation/clean.sh
+++ src/simulation/clean.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Loop through all items in the current directory
-for item in *; do
-  # Check if the item is a directory
-  if [ -d "$item" ]; then
-    # Remove the directory and its contents
-    rm -rf "$item"
-    echo "Removed directory: $item"
-  fi
-done
+cd result
+shopt -s extglob
+rm -rf !(result.ipynb|README.md)
+cd ..
