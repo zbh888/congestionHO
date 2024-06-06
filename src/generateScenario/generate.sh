f3cc18f09dde8debf40debeb7c809e9ddf3d2a5a
Bohan Zhang <b327zhan@uwaterloo.ca>
2024-06-06 18:53:05 -0400

    Update generate.sh

--- src/generateScenario/generate.sh
+++ src/generateScenario/generate.sh
@@ -4,7 +4,7 @@ start_time=$(date +%s)
 
 g++ -std=c++11 -o runcpp generateScenario.cpp -Iinclude
 ./runcpp
-python3 load_save.py
+python3 -u load_save.py
 python3 draw_scenario.py
 rm *bin
 rm duration.txt
