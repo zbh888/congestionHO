353e2f454e3f70cde2a6594a3fde192dc8753dc6
Bohan Zhang <b327zhan@uwaterloo.ca>
2024-06-26 17:01:24 -0400

    Update Satellite.py

--- src/simulation/Satellite.py
+++ src/simulation/Satellite.py
@@ -485,6 +485,7 @@ class Satellite(Base):
             max_values = np.max(A_valid, axis=0)
             min_value = np.min(max_values)
             min_indices = np.where(max_values == min_value)[0]
+            print(f"available slots: {min_indices}")
             weights = np.arange(len(min_indices), 0, -1) ** BIAS_FACTOR
             weights = weights / np.sum(weights)
             random_min_index = np.random.choice(min_indices, p=weights)
